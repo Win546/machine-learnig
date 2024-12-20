56934622c18ae9fc25a5732efa10f6d0f696cc48
Vincenzo <91315687+Win546@users.noreply.github.com>
2024-12-20 18:00:52 +0100

    Update sketchPad.js

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -9,7 +9,7 @@ class SketchPad {
         `;
         container.appendChild(this.canvas);
         this.ctx = this.canvas.getContext("2d");
-        this.path = [];
+        this.paths = [];
         this.isDrawing = false;
 
 
@@ -20,7 +20,7 @@ class SketchPad {
         this.canvas.onmousedown = (evt) => {
             const mouse = this.#getMouse(evt);
 
-            this.path =[mouse];
+            this.paths.push([mouse]);
             this.isDrawing = true;
         };
 
@@ -28,9 +28,9 @@ class SketchPad {
             if (this.isDrawing) {
                 const mouse = this.#getMouse(evt);
 
-
-                this.path.push(mouse);
-               console.log(this.path.length);
+                const lastPath=this.paths[this.paths.length-1]
+               lastPath.push(mouse);
+              
                 this.#redraw();
 
             }
@@ -44,7 +44,7 @@ class SketchPad {
     #redraw() {
         this.ctx.clearRect(0, 0,
                 this.canvas.width, this.canvas.height);
-        draw.path(this.ctx, this.path);
+        draw.paths(this.ctx, this.paths);
     }
 
     #getMouse = (evt) => {
